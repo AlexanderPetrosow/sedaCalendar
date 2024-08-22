f376e7085bd179ac41d581ddbde3e48661c6b76b
Mekan Agamyradov <m.agamyradov@lotta.local>
2024-08-22 10:22:34 +0500

    edit home panel design

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,90 +14,90 @@
         </div>
 
         <div class="flex justify-center items-center w-full">
-            <div class="flex justify-around flex-wrap gap-10 md:gap-16 ">
-                <div class="flex flex-col w-[38%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
-                    <div class="flex flex-row gap-4 pt-6 pb-2 px-4 bg-gray-300">
+            <div class="flex flex-wrap justify-around gap-2 ">
+                <div class="flex flex-col w-[42%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
+                    <div class="flex flex-row gap-4 p-2 bg-gray-300 ">
                         <div>
                             <i class="bx bx-book-open text-[24px]"></i>
                         </div>                        
                         <div>@lang('nav.admin_poem')</div>
                     </div>
-                    <div class="pt-6 pb-6 px-4 text-white text-justify">
+                    <div class="px-2 text-justify text-white flex-1">
                         Bu bölümde Siz web sahypasynda gorkezilýän goşgulary goşup, goşulan goşgulary üýtgedip, bozup hem-de şol bir wagtda statusyny hem üýtgedip bilersiňiz
                     </div>
-                    <div class="flex justify-between p-2 border-t-2">
+                    <div class="flex justify-between border-t-2">
                         <div class="flex justify-center flex-1 text-white">
                             {{$poemCount}} poems
                         </div>
-                        <div class="flex flex-1 justify-center items-center text-white">
-                            <a class="flex justify-center items-center" href="{{route('admin.poem')}}">
+                        <div class="flex items-center justify-center flex-1 text-white">
+                            <a class="flex items-center justify-center text-nowrap " href="{{route('admin.poem')}}">
                                 <i class='bx bx-show-alt text-[30px] mr-2'> </i> giňişleýin görmek
                             </a>
                         </div>
                     </div>
                 </div>
                     
-                <div class="flex flex-col w-[38%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
-                    <div class="flex flex-row gap-4 pt-6 pb-2 px-4 bg-gray-300">
+                <div class="flex flex-col w-[42%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
+                    <div class="flex flex-row gap-4 p-2 bg-gray-300 ">
                         <div>
                             <i class="bx bx-music text-[24px]"></i>
                         </div>                        
                         <div>@lang('nav.admin_audio_poem')</div>
                     </div>
-                    <div class="pt-6 pb-6 px-4 text-white text-justify">
+                    <div class="px-2 text-justify text-white flex-1">
                         Bu bölümde Siz web sahypasynda gorkezilýän okalan audio goşgulary goşup, goşulan audio goşgulary üýtgedip, bozup hem-de şol bir wagtda statusyny hem üýtgedip bilersiňiz
                     </div>
-                    <div class="flex justify-between p-2 border-t-2">
+                    <div class="flex justify-between border-t-2">
                         <div class="flex justify-center flex-1 text-white">
                             {{$audioCount}} audio poem
                         </div>
-                        <div class="flex flex-1 justify-center items-center text-white">
-                            <a class="flex justify-center items-center" href="{{route('admin.audioPoem')}}">
+                        <div class="flex items-center justify-center flex-1 text-white">
+                            <a class="flex items-center justify-center text-nowrap" href="{{route('admin.audioPoem')}}">
                                 <i class='bx bx-show-alt text-[30px] mr-2'> </i> giňişleýin görmek
                             </a>
                         </div>
                     </div>
                 </div>
                     
-                <div class="flex flex-col w-[38%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
-                    <div class="flex flex-row gap-4 pt-6 pb-2 px-4 bg-gray-300">
+                <div class="flex flex-col w-[42%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
+                    <div class="flex flex-row gap-4 p-2 bg-gray-300 ">
                         <div>
                             <i class="bx bx-photo-album text-[24px]"></i>
                         </div>                        
                         <div>@lang('nav.admin_gallery')</div>
                     </div>
-                    <div class="pt-6 pb-6 px-4 text-white text-justify">
+                    <div class="px-2 text-justify text-white flex-1">
                         Bu bölümde Siz web sahypasynda gorkezilýän suratlary goşup, bozup hem-de şol bir wagtda statusyny hem üýtgedip bilersiňiz
                     </div>
-                    <div class="flex justify-between p-2 border-t-2">
+                    <div class="flex justify-between border-t-2">
                         <div class="flex justify-center flex-1 text-white">
                             {{$galleryCount}} galereýa
                         </div>
-                        <div class="flex flex-1 justify-center items-center text-white">
-                            <a class="flex justify-center items-center" href="{{route('admin.gallery')}}">
+                        <div class="flex items-center justify-center flex-1 text-white">
+                            <a class="flex items-center justify-center text-nowrap" href="{{route('admin.gallery')}}">
                                 <i class='bx bx-show-alt text-[30px] mr-2'> </i> giňişleýin görmek
                             </a>
                         </div>
                     </div>
                 </div>
                     
-                <div class="flex flex-col w-[38%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
-                    <div class="flex flex-row gap-4 pt-6 pb-2 px-4 bg-gray-300">
+                <div class="flex flex-col w-[42%] bg-gray-400 rounded-md overflow-hidden gap-4 ">
+                    <div class="flex flex-row gap-4 p-2 bg-gray-300 ">
                         <div>
                             <i class="bx bx-user-plus text-[24px]"></i>
                         </div>                        
                         <div>@lang('nav.admin_create')</div>
                     </div>
-                    <div class="pt-6 pb-6 px-4 text-white text-justify">
+                    <div class="px-2 text-justify text-white flex-1">
                         Bu bölümde Siz web sahypasyny dolandyrmak üçin admin doredip, üýtgedip hemde bozup bilersiňiz!
                     </div>
-                    <div class="flex justify-between p-2 border-t-2">
+                    <div class="flex justify-between border-t-2">
                         <div class="flex justify-center flex-1 text-white">
                             {{$userCount - 1}} admin
                         </div>
-                        <div class="flex flex-1 justify-center items-center text-white">
+                        <div class="flex items-center justify-center flex-1 text-white">
                             @if(auth()->user()->is_admin == 1)
-                                <a class="flex justify-center items-center" href="{{route('admin.controll')}}">
+                                <a class="flex items-center justify-center" href="{{route('admin.controll')}}">
                                     <i class='bx bx-show-alt text-[30px] mr-2'> </i> giňişleýin görmek
                                 </a>
                             @else
@@ -107,8 +107,6 @@
                     </div>
                 </div>
             </div>
-            
-
         </div>
         
     </div>
